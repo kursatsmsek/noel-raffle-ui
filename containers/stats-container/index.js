1e20ed08b278ddb33aa7811e4314974020fe6646
kursatsmsek <kursatsimsek@protonmail.ch>
2023-12-04 19:38:44 +0300

    stats page images added

--- containers/stats-container/index.js
+++ containers/stats-container/index.js
@@ -3,6 +3,11 @@ import styles from "./styles.module.css";
 import { Mountains_of_Christmas } from "next/font/google";
 import StatBox from "@/components/stat-box";
 import { Grid } from "@mui/material";
+import SnowMan from "@/assets/logo2.png";
+import Cookie from "@/assets/cookie.png";
+import GiftHand from "@/assets/gift-hand.png";
+import FatherChristmas from "@/assets/logo1.png";
+import Image from "next/image";
 
 const font = Mountains_of_Christmas({ subsets: ["latin"], weight: "700" });
 
@@ -13,7 +18,7 @@ function StatsContainer({ page }) {
         item
         xs={4}
         sm={8}
-        md={6}
+        md={4}
         style={{ display: "flex", justifyContent: "center" }}
       >
         <StatBox number={100} description={"Toplam Çekiliş"} />
@@ -22,7 +27,43 @@ function StatsContainer({ page }) {
         item
         xs={4}
         sm={8}
-        md={6}
+        md={4}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Image width={150} src={FatherChristmas} alt="father-christmas" />
+      </Grid>
+      <Grid
+        item
+        xs={4}
+        sm={8}
+        md={4}
+        style={{ display: "flex", justifyContent: "center" }}
+      >
+        <StatBox number={99} description={"Toplam Hediye"} />
+      </Grid>
+
+      <Grid
+        item
+        xs={4}
+        sm={8}
+        md={4}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Image width={150} src={SnowMan} alt="snow-man" />
+      </Grid>
+      <Grid
+        item
+        xs={4}
+        sm={8}
+        md={4}
         style={{ display: "flex", justifyContent: "center" }}
       >
         <StatBox number={99} description={"Toplam Hediye"} />
@@ -31,16 +72,21 @@ function StatsContainer({ page }) {
         item
         xs={4}
         sm={8}
-        md={12}
-        style={{ display: "flex", justifyContent: "center" }}
+        md={4}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
       >
-        <StatBox number={99} description={"Toplam Hediye"} />
+        <Image width={150} src={Cookie} alt="cookie-man" />
       </Grid>
+
       <Grid
         item
         xs={4}
         sm={8}
-        md={6}
+        md={4}
         style={{ display: "flex", justifyContent: "center" }}
       >
         <StatBox number={100} description={"Toplam Çekiliş"} />
@@ -49,7 +95,20 @@ function StatsContainer({ page }) {
         item
         xs={4}
         sm={8}
-        md={6}
+        md={4}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Image width={150} src={GiftHand} alt="gift-hand" />
+      </Grid>
+      <Grid
+        item
+        xs={4}
+        sm={8}
+        md={4}
         style={{ display: "flex", justifyContent: "center" }}
       >
         <StatBox number={99} description={"Toplam Hediye"} />
